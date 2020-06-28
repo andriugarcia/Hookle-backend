04164ddf944ead0d7d71385b557ffa3a075b9e5d
VDrews <thedrews@outlook.es>
2020-06-28 21:59:36 +0200

    First commit

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var neo4j = require('neo4j-driver');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const service=require('./services')
 const queries=require('./queries');
@@ -27,57 +29,189 @@ app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'A4sJ2eG4aM4s'));
-var session = driver.session();
+// var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'A4sJ2eG4aM4s'));
+var driver = neo4j.driver('bolt://18.203.12.204', neo4j.auth.basic('neo4j', 'A4sJ2eG4aM4s'));
+
+Object.defineProperty(Array.prototype, 'shuffle', {
+  value: function () {
+    for (let i = this.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this[i], this[j]] = [this[j], this[i]];
+    }
+    return this;
+  }
+});
 
 app.get('/stack', async (req,res) => {
   try {
     let payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
-    let result = await session.run(queries.stack, { emailParam: payload })
-    
-    if (result.records.length == 0) return res.sendStatus(401);
-    let nodes = result.records.map(record => (record._fields[0].properties))
-    console.log(payload);
-    const length = nodes.length;
-
-    if (length < 20) {
-      console.log("Mixing with populars", length)
-      const limit = 20 - length
-      console.log("LIMIT: ", limit)
-      let popularResult = await session.run(queries.populars, {
-        limit: neo4j.int(limit),
-        emailParam: payload
-      })
-      nodes = [
-        ...nodes,
-        ...popularResult.records.map(popularRecord => (popularRecord._fields[0].properties))
-      ]
-      console.log(nodes)          
-      res.status(201).send(nodes)
-    }
-    else {
-      console.log("No mix", length)
-      console.log(nodes)          
-      res.status(200).send(nodes)
-    }
-
-  }
-  catch(err) {
+    let session = driver.session();
+    let result = await session.run(queries.stackRating, { emailParam: payload })
+    let populars = await session.run(queries.popularsRating, { emailParam: payload })
+    let random = await session.run(queries.random)
+    session.close()
+  
+    let nodes = [
+      ...result.records.map(record => (record._fields[0].properties)),
+      ...populars.records.map(record => (record._fields[0].properties)),
+      ...random.records.map(record => (record._fields[0].properties))
+    ].shuffle()
+    res.status(201).send(nodes)
+  } catch (err) {
     console.error(err);
     res.sendStatus(401);
   }
-})
+});
+
+app.get('/', async (req,res) => {
+  try {
+    let session = driver.session();
+    await session.run(`MATCH (u: User{ email: $emailParam })
+                      MATCH (c: Clothing{ code: $clothingParam })
+                      CREATE (u)-[r:LIKES {rating: $rate}]->(c)`, {
+      emailParam: "u1@gmail.com",
+      clothingParam: "B079RST3NH",
+      rate: neo4j.int(Math.round(Math.random() * 10))
+    })
+    await session.run(`MATCH (u: User{ email: $emailParam })
+                      MATCH (c: Clothing{ code: $clothingParam })
+                      CREATE (u)-[r:LIKES {rating: $rate}]->(c)`, {
+      emailParam: "u1@gmail.com",
+      clothingParam: "B086882SNQ",
+      rate: neo4j.int(Math.round(Math.random() * 10))
+    })
+    await session.run(`MATCH (u: User{ email: $emailParam })
+                      MATCH (c: Clothing{ code: $clothingParam })
+                      CREATE (u)-[r:LIKES {rating: $rate}]->(c)`, {
+      emailParam: "u1@gmail.com",
+      clothingParam: "B07XJ8RVXT",
+      rate: neo4j.int(Math.round(Math.random() * 10))
+    })
+    await session.run(`MATCH (u: User{ email: $emailParam })
+                      MATCH (c: Clothing{ code: $clothingParam })
+                      CREATE (u)-[r:LIKES {rating: $rate}]->(c)`, {
+      emailParam: "u1@gmail.com",
+      clothingParam: "B07K2N2163",
+      rate: neo4j.int(Math.round(Math.random() * 10))
+    })
+    await session.run(`MATCH (u: User{ email: $emailParam })
+                      MATCH (c: Clothing{ code: $clothingParam })
+                      CREATE (u)-[r:LIKES {rating: $rate}]->(c)`, {
+      emailParam: "u1@gmail.com",
+      clothingParam: "B072JGS75F",
+      rate: neo4j.int(Math.round(Math.random() * 10))
+    })
+    await session.run(`MATCH (u: User{ email: $emailParam })
+                      MATCH (c: Clothing{ code: $clothingParam })
+                      CREATE (u)-[r:LIKES {rating: $rate}]->(c)`, {
+      emailParam: "u1@gmail.com",
+      clothingParam: "B07SND6DCH",
+      rate: neo4j.int(Math.round(Math.random() * 10))
+    })
+    await session.run(`MATCH (u: User{ email: $emailParam })
+                      MATCH (c: Clothing{ code: $clothingParam })
+                      CREATE (u)-[r:LIKES {rating: $rate}]->(c)`, {
+      emailParam: "u1@gmail.com",
+      clothingParam: "B085RSDLN1",
+      rate: neo4j.int(Math.round(Math.random() * 10))
+    })
+    await session.run(`MATCH (u: User{ email: $emailParam })
+                      MATCH (c: Clothing{ code: $clothingParam })
+                      CREATE (u)-[r:LIKES {rating: $rate}]->(c)`, {
+      emailParam: "u1@gmail.com",
+      clothingParam: "B07Y5BVWP3",
+      rate: neo4j.int(Math.round(Math.random() * 10))
+    })
+    await session.run(`MATCH (u: User{ email: $emailParam })
+                      MATCH (c: Clothing{ code: $clothingParam })
+                      CREATE (u)-[r:LIKES {rating: $rate}]->(c)`, {
+      emailParam: "u1@gmail.com",
+      clothingParam: "B07V8HBVBT",
+      rate: neo4j.int(Math.round(Math.random() * 10))
+    })
+
+    session.close()  
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+});
+
+app.get('/populars', async (req,res) => {
+  try {
+    let payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
+    let session = driver.session();
+    let result = await session.run(queries.popularsRating, { emailParam: payload })
+    session.close()
+  
+    let nodes = result.records.map(record => (record._fields[0].properties))
+    res.status(201).send(nodes)
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+});
+
+// app.get('/stack', async (req,res) => {
+//   console.log("1")
+//   try {
+//     let payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
+//     console.log("2", payload)
+//     let session1 = driver.session();
+//     let result = await session1.run(queries.stack, { emailParam: payload })
+//     session1.close()
+    
+    
+//     let session2 = driver.session();
+//     let random = await session2.run(queries.random) // Prendas todavía sin descubrir
+//     session2.close()
+    
+//     let nodes = result.records.map(record => (record._fields[0].properties))
+//     let randomnodes = random.records.map(record => (record._fields[0].properties))
+//     const length = nodes.length;
+    
+//     if (length < 20) {
+//       const limit = 20 - length
+//       let session3 = driver.session();
+//       let popularResult = await session3.run(queries.populars, {
+//         limit: neo4j.int(limit),
+//         emailParam: payload
+//       })
+//       session3.close()
+//       console.log(nodes.length, randomnodes.length, popularResult.records.length)
+//       nodes = [
+//         ...nodes,
+//         ...randomnodes,
+//         ...popularResult.records.map(popularRecord => (popularRecord._fields[0].properties))
+//       ]
+//       res.status(201).send(nodes)
+//     }
+//     else {
+//       console.log("No mix", length)
+//       console.log(nodes.length, randomnodes.length, 'No populars')          
+//       res.status(200).send([...nodes, ...randomnodes])
+//     }
+
+//   }
+//   catch(err) {
+//     console.error(err);
+//     res.sendStatus(401);
+//   }
+// })
     
 app.post('/signup', async (req, res) => {
   const user={
     email: req.body.email,
     password: req.body.password
   }
-  
+
+  let session = driver.session();
   await session.run(queries.signup, {
-        emailParam: user.email,
-        passParam: user.password
-      })
+    emailParam: user.email,
+    passParam: user.password
+  })
+  session.close()
       
   res.status(200).send({
     message: 'User created',
@@ -86,7 +220,10 @@ app.post('/signup', async (req, res) => {
 })
 
 app.post('/signin', async (req, res) => {
+  let session = driver.session();
   const result = await session.run(queries.signin, { emailParam: req.body.email })
+  session.close()
+
   if (result.records.length == 0) {
     res.sendStatus(404);
     return;
@@ -111,7 +248,9 @@ app.get('/likes', async (req, res) => {
   try {
     const payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
     console.log(payload)
+    let session = driver.session();
     const result = await session.run(queries.likes, { emailParam: payload })
+    session.close()
     let nodes = result.records.map(record => (record._fields[0].properties))
     res.send(nodes);
   } catch(err) {
@@ -124,7 +263,9 @@ app.get('/dislikes', async (req, res) => {
   try {
     const payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
       console.log(payload)
+      let session = driver.session();
       const result = await session.run(queries.dislikes, { emailParam: payload })
+      session.close()
       let nodes = result.records.map(record => (record._fields[0].properties))
       res.send(nodes);            
   }
@@ -138,7 +279,9 @@ app.get('/favorites', async (req, res) => {
   try {
     const payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
       console.log(payload)
+      let session = driver.session();
       const result = await session.run(queries.favorites, { emailParam: payload })
+      session.close()
       let nodes = result.records.map(record => (record._fields[0].properties))
       res.send(nodes);
             
@@ -152,10 +295,32 @@ app.get('/bought', async (req, res) => {
   try {
     const payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
     console.log(payload)
+    let session = driver.session();
     const result = await session.run(queries.bought, { emailParam: payload })      
+    session.close()
     let nodes = result.records.map(record => (record._fields[0].properties))
     res.send(nodes);
   } catch(err){
+    console.error(err);
+    res.send(401);
+  }
+})
+
+app.post('/vote', async (req, res) => {
+  try {
+    const payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
+    console.log(payload)
+    let session = driver.session();
+    await session.run(queries.vote, {
+      emailParam: payload,
+      clothingParam: req.body.clothing,
+      ratingParam: req.body.rating
+    })      
+    session.close()
+    console.log("Relacionado exitosamente")
+    res.sendStatus(200);
+          
+  } catch(err) {
     console.error(err);
     res.send(401);
   }
@@ -165,10 +330,12 @@ app.post('/like', async (req, res) => {
   try {
     const payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
     console.log(payload)
+    let session = driver.session();
     await session.run(queries.like, {
-                emailParam: payload,
-                clothingParam: req.body.clothing
-              })      
+      emailParam: payload,
+      clothingParam: req.body.clothing
+    })      
+    session.close()
     console.log("Relacionado exitosamente")
     res.sendStatus(200);
           
@@ -183,10 +350,12 @@ app.post('/dislike', async (req, res) => {
   try {
     const payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
     console.log(payload)
+    let session = driver.session();
     await session.run(queries.dislike, {
-                emailParam: payload,
-                clothingParam: req.body.clothing
-              })
+      emailParam: payload,
+      clothingParam: req.body.clothing
+    })
+    session.close()
     console.log("Relacionado exitosamente")
     res.sendStatus(200);
   } catch(err) {
@@ -199,10 +368,12 @@ app.post('/fav', async (req, res) => {
   try {
     const payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
       console.log(payload)
+      let session = driver.session();
       await session.run(queries.favorite, {
-                  emailParam: payload,
-                  clothingParam: req.body.clothing
-                })
+        emailParam: payload,
+        clothingParam: req.body.clothing
+      })
+      session.close()
       console.log("Relacionado exitosamente")
       res.sendStatus(200);
 
@@ -216,16 +387,53 @@ app.post('/buy', async (req, res) => {
   try {
     const payload = await service.decodeToken(req.headers.authorization.split(" ")[1])
       console.log(payload)
+      let session = driver.session();
       await session.run(queries.buy, {
-                  emailParam: payload,
-                  clothingParam: req.body.clothing
-                })      
+        emailParam: payload,
+        clothingParam: req.body.clothing
+      })      
+      session.close()
       console.log("Relacionado exitosamente")
       res.sendStatus(200);
   } catch(err) {
     console.error(err);
     res.send(401);
   }
+})
+
+app.get('/product/:code', async (req, res) => {
+  // try {
+    const url = `http://www.amazon.es/dp/${req.params.code}`;
+
+    const {
+      data
+    } = await axios.get(url);
+
+    // Price
+    const $ = cheerio.load(data);
+    const price = $('#priceblock_ourprice').get([0]);
+
+    // Description
+    let featureDetails = [];
+    $('#feature-bullets>ul>li>span').each((i, el) => {
+      featureDetails.push($(el).text().replace(/[\n\t]/g, '').trim());
+    })
+    let description = featureDetails.join('\n')
+
+    // Image
+    const images = $('#landingImage ').attr('data-old-hires');
+    let details = {
+      price: price.children[0].data,
+      pictures: [images],
+      description
+    }
+    console.log(details)
+    res.send(details)
+
+  // } catch (err) {
+  //   console.error(err);
+  //   res.sendStatus(404);
+  // }
 })
 
 app.listen(3010);
--- queries.js
+++ queries.js
@@ -1,8 +1,8 @@
 
 const stack = `MATCH (u:User {email: $emailParam})-[:LIKES]->(lc:Clothing)<-[:LIKES]-(l)-[:LIKES]->(like)
-              WHERE NOT (u)-[:LIKES]->(like) AND NOT (u)-[:DISLIKES]->(like)
+              WHERE NOT (like)<-[]-(u)
               MATCH (u)-[:DISLIKES]->(dc:Clothing)<-[:DISLIKES]-(d)-[:LIKES]->(dislike)
-              WHERE NOT (u)-[:LIKES]->(dislike) AND NOT (u)-[:DISLIKES]->(dislike)
+              WHERE NOT (dislike)<-[]-(u)
               WITH [like, dislike] as results
               UNWIND results AS x
               UNWIND x AS y
@@ -10,12 +10,92 @@ const stack = `MATCH (u:User {email: $emailParam})-[:LIKES]->(lc:Clothing)<-[:LI
               ORDER BY frequency DESC
               LIMIT 20`;
 
+const stackRating = `MATCH (u1:User {email:$emailParam})-[r:LIKES]->(m:Clothing)
+                    WITH u1, avg(r.rating) AS u1_mean
+
+                    MATCH (u1)-[r1:LIKES]->(m:Clothing)<-[r2:LIKES]-(u2)
+                    WITH u1, u1_mean, u2, COLLECT({r1: r1, r2: r2}) AS ratings
+
+                    MATCH (u2)-[r:LIKES]->(m:Clothing)
+                    WITH u1, u1_mean, u2, avg(r.rating) AS u2_mean, ratings
+
+                    UNWIND ratings AS r
+
+                    WITH sum( (r.r1.rating-u1_mean) * (r.r2.rating-u2_mean) ) AS nom,
+                        sqrt( sum( (r.r1.rating - u1_mean)^2) * sum( (r.r2.rating - u2_mean) ^2)) AS denom,
+                        u1, u2 WHERE denom <> 0
+
+                    WITH u1, u2, nom/denom AS pearson
+                    ORDER BY pearson DESC LIMIT 10
+
+                    MATCH (u2)-[r:LIKES]->(m:Clothing) WHERE NOT EXISTS( (u1)-[:LIKES]->(m) )
+
+                    RETURN m, SUM( pearson * r.rating) AS score
+                    ORDER BY score DESC LIMIT 25`;
+
 const populars = `MATCH (a:User)-[r:LIKES]->(c:Clothing)
-                  WHERE NOT (:User {email: $emailParam})-[:LIKES]->(c) AND NOT (:User {email: $emailParam})-[:DISLIKES]->(c)
+                  WHERE NOT (:User {email: $emailParam})-[]->(c)
                   RETURN c, SIZE(COLLECT(a)) as likes
-                  ORDER BY likes DESC LIMIT $limit`;
+                  ORDER BY likes DESC LIMIT 20`;
+
+const popularsRating = `MATCH (a:User)-[r:LIKES]->(c:Clothing)
+                        WHERE NOT (:User {email: $emailParam})-[]->(c)
+                        WITH c, COLLECT(r.rating) as likes
+                        UNWIND likes AS likesret
+                        RETURN c, AVG(likesret) AS len
+                        ORDER BY len DESC LIMIT 10`;
+
+const random = `MATCH   (n:Clothing)
+                WHERE   (NOT (n)-[]-())
+                WITH n, rand() AS number
+                RETURN n 
+                ORDER BY number
+                LIMIT 5`;
+        
+// MATCH(u) - [: DISLIKES] - > (dc: Clothing) < -[: DISLIKES] - (d) - [: LIKES] - > (dislike)
+// WHERE NOT(dislike) < -[] - (u)
+const fullstack = `MATCH (u:User {email: $emailParam})-[:LIKES]->(lc:Clothing)<-[:LIKES]-(l)-[:LIKES]->(like)
+                  WHERE NOT EXISTS ( (like)<-[]-(u) )
+                  WITH [like] as results
+                  UNWIND results AS x
+                  UNWIND x AS y
+                  WITH y, count(y) as frequency
+                  ORDER BY frequency DESC
+                  LIMIT 20 RETURN y
+
+                  UNION
+
+                  MATCH   (n:Clothing)
+                  WHERE NOT EXISTS ( (n)<-[]-() )
+                  WITH n, rand() AS number
+                  RETURN n AS y
+                  ORDER BY number
+                  LIMIT 5`;
 
 const signup = 'CREATE(u:User {email: $emailParam, password: $passParam}) RETURN u.email';
+
+`MATCH (u1:User {email:"u1@gmail.com"})-[r:LIKES]->(m:Clothing)
+WITH u1, avg(r.rating) AS u1_mean
+
+MATCH (u1)-[r1:LIKES]->(m:Clothing)<-[r2:LIKES]-(u2)
+WITH u1, u1_mean, u2, COLLECT({r1: r1, r2: r2}) AS ratings WHERE size(ratings) > 10
+
+MATCH (u2)-[r:LIKES]->(m:Clothing)
+WITH u1, u1_mean, u2, avg(r.rating) AS u2_mean, ratings
+
+UNWIND ratings AS r
+
+WITH sum( (r.r1.rating-u1_mean) * (r.r2.rating-u2_mean) ) AS nom,
+     sqrt( sum( (r.r1.rating - u1_mean)^2) * sum( (r.r2.rating - u2_mean) ^2)) AS denom,
+     u1, u2 WHERE denom <> 0
+
+WITH u1, u2, nom/denom AS pearson
+ORDER BY pearson DESC LIMIT 10
+
+MATCH (u2)-[r:LIKES]->(m:Clothing) WHERE NOT EXISTS( (u1)-[:LIKES]->(m) )
+
+RETURN m.title, SUM( pearson * r.rating) AS score
+ORDER BY score DESC LIMIT 25`
 
 const signin = 'MATCH (u:User { email: $emailParam }) RETURN u';
 
@@ -27,21 +107,24 @@ const favorites = `MATCH (n:User {email: $emailParam})-[r:FAVORITE]->(c)
               RETURN c`;
 const bought = `MATCH (n:User {email: $emailParam})-[r:BUY]->(c)
               RETURN c`;
+const vote = `MATCH (u: User{ email: $emailParam })
+              MATCH (c: Clothing{ code: $clothingParam })
+              CREATE (u)-[r:LIKES {rating: $ratingParam}]->(c)`
 const like = `MATCH (u: User{ email: $emailParam })
-              MATCH (c: Clothing{ name: $clothingParam })
+              MATCH (c: Clothing{ code: $clothingParam })
               CREATE (u)-[r:LIKES]->(c)`
 const dislike = `MATCH (u: User{ email: $emailParam })
-              MATCH (c: Clothing{ name: $clothingParam })
+              MATCH (c: Clothing{ code: $clothingParam })
               CREATE (u)-[r:DISLIKES]->(c)`
 const favorite = `MATCH (u: User{ email: $emailParam })
-              MATCH (c: Clothing{ name: $clothingParam })
+              MATCH (c: Clothing{ code: $clothingParam })
               CREATE (u)-[r:FAVORITE]->(c)`
 const buy = `MATCH (u: User{ email: $emailParam })
-              MATCH (c: Clothing{ name: $clothingParam })
+              MATCH (c: Clothing{ code: $clothingParam })
               CREATE (u)-[r:BUY]->(c)`
 
 
 
 module.exports = {
-  stack, populars, signup, signin, likes, dislikes, favorites, bought, like, dislike, favorite, buy
+  stack, populars, signup, signin, likes, dislikes, favorites, bought, like, dislike, favorite, buy, random, fullstack, vote, stackRating, popularsRating
 }
