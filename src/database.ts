12e060a1a8feb53ad1855e234ec1f8c3df7ba9a8
VDrews <thedrews@outlook.es>
2020-11-18 18:27:01 +0100

    Actualizacion para que Alex lo revise

--- src/database.ts
+++ src/database.ts
@@ -3,8 +3,9 @@ import neo4j from 'neo4j-driver'
 
 dotenv.config({ path: __dirname + '/.env' })
 
+// 'bolt://18.203.12.204', production
 var driver = neo4j.driver(
-    'bolt://18.203.12.204',
+    'bolt://localhost:7687',
     neo4j.auth.basic(process.env.DATABASE_USER, process.env.DATABASE_PASS)
 )
 
